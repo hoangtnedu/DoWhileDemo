c0f9ca449d18cd3eb858c1a35e4f83e598cc3b41
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-08-13 14:52:26 +0700

    Initial commit

---
+++ src/dowhiledemo/DoWhileDemo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dowhiledemo;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class DoWhileDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 11);
+    }
+    
+}
